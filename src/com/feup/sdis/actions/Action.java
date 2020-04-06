26c35d0d39dd520930cf908c9787b74dec6418fb
GambuzX <mg_mesquita@hotmail.com>
2020-04-06 16:58:37 +0100

    File IDs are now SHA-256 Hashes

--- src/com/feup/sdis/actions/Action.java
+++ src/com/feup/sdis/actions/Action.java
@@ -2,8 +2,22 @@ package com.feup.sdis.actions;
 
 import com.feup.sdis.peer.Constants;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public interface Action {
     static String generateId(String fileContent) {  // TODO: Check actual algorithm for file storage
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(fileContent.getBytes(StandardCharsets.UTF_8));
+            String hex = String.format("%064x", new BigInteger(1, hash));
+            return hex;
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
         return  Integer.toString((fileContent + Constants.SENDER_ID).hashCode());
     }
 
