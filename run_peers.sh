e8efe76c621dfab6cabf30ec5cb3f77e0c89a95f
GambuzX <mg_mesquita@hotmail.com>
2020-04-14 17:48:40 +0100

    default values in peers script

--- run_peers.sh
+++ run_peers.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # print usage
-if [ "$#" != 8 ]; then
-  echo "Usage: $0 N_PEERS VERSION MC_ADDR MC_PORT MDB_ADDR MDB_PORT MDR_ADDR MDR_PORT"
+if [ "$#" != 1 -a "$#" != 2 -a "$#" != 8 ]; then
+  echo "Usage: $0 N_PEERS <VERSION> <MC_ADDR MC_PORT MDB_ADDR MDB_PORT MDR_ADDR MDR_PORT>"
   echo "  N_PEERS is the number of peers to start"
   echo "    - IDs and Access Points will be assigned incrementally, starting at 1"
   echo "    - For example, N_PEERS = 4 starts peers with IDs and Access Points 1,2,3,4"
   echo "  VERSION is the peer protocol version, 1.0 is base, 1.1 is enhanced. By default its 1.0."
+  echo ""
+  echo "  The remaining arguments are optional, but must be given together: "
   echo "  MC_ADDR is the multicast address to be used for the MC channel"
   echo "  MC_PORT is the port to be used for the MC channel"
   echo "  MDB_ADDR is the multicast address to be used for the MDB channel"
@@ -16,15 +18,36 @@ if [ "$#" != 8 ]; then
   exit 1
 fi
 
+# default values
+N_PEERS=$1
+VERSION='1.0'
+MC_ADDR='224.0.0.0'
+MC_PORT=8080
+MDB_ADDR='224.0.0.1'
+MDB_PORT=8080
+MDR_ADDR='224.0.0.2'
+MDR_PORT=8080
+
+if [ "$#" == 2 ]; then
+  VERSION=$2
+elif [ "$#" == 8 ]; then
+  VERSION=$2
+  MC_ADDR=$3
+  MC_PORT=$4
+  MDB_ADDR=$5
+  MDB_PORT=$6
+  MDR_ADDR=$7
+  MDR_PORT=$8
+fi
+
 # create output folder
 mkdir outputs &> /dev/null
 
 # start processes
-N_PEERS=$1
 echo "Starting $N_PEERS peers"
 for (( id=1; id<=$N_PEERS; id++ ))
 do
-    (java -cp out/production/sdis1920-t1g02/ com.feup.sdis.peer.Peer $2 $id $id $3 $4 $5 $6 $7 $8 &>> outputs/p$id.txt)&
+    (java -cp out/production/sdis1920-t1g02/ com.feup.sdis.peer.Peer $VERSION $id $id $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT &>> outputs/p$id.txt)&
     pids[$id]=$!
 done
 
